c8ff6ec3874b1c917a7847f922a88d24980197ee
Artur Bauer <bauerart1@gmail.com>
2020-10-09 23:31:24 +0200

    fix website removing

--- .circleci/deployDocs.sh
+++ .circleci/deployDocs.sh
@@ -14,12 +14,12 @@ mv build/clamp-orchestrator/* ../_site
 # Run commands inside root directory
 cd ..
 git checkout gh-pages
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec git rm -rf {} \;
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
 mv ./_site/* .
 rm -R _site/
 
 git add -fA
 git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
-git push -qf https://${DOCS_GITHUB_TOKEN}@github.com/ArturBa/clamp-orchestrator.git ph-pages
+git push -q https://${DOCS_GITHUB_TOKEN}@github.com/ArturBa/clamp-orchestrator.git ph-pages
 
 echo "documentation updated successfully"
