e9597d52f383a4f7dc7312c8399f18d835ee357a
Artur Bauer <bauerart1@gmail.com>
2020-10-09 22:18:05 +0200

    Update deployDocs to use gh-pages branch

--- .circleci/deployDocs.sh
+++ .circleci/deployDocs.sh
@@ -5,11 +5,19 @@ git config user.email "$(git log master -1 --pretty=%ae)"
 git checkout master
 git pull origin master
 
+# Run commands inside webside directory
 cd website
 npm run build
-mv docs/* ./_site
-rm -R docs/
+mkdir ./_site
+mv build/* ./_site
 
+# Run commands inside root directory
+cd ..
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
+mv ./_site/* .
+rm -R _site/
+
+git checkout gh-pages
 git add -fA
 git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
 git push -f origin master
